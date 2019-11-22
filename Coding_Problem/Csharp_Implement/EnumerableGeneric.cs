6cf718a21d115c5891c14dd79063ce87b4d6cdab
surplusus <moing6862@naver.com>
2019-11-22 12:48:24 +0900

    저장을 생활활하자

---
+++ Coding_Problem/Csharp_Implement/EnumerableGeneric.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Implement
+{
+    class MyList<T> : IEnumerable<T>, IEnumerator<T>
+    {
+        private T[] array;
+        int position = -1;
+
+        public MyList()
+        {
+            array = new T[3];
+        }
+        public T this[int idx]
+        {
+            get
+            {
+                return array[idx];
+            }
+            set
+            {
+                if (idx <= array.Length)
+                {
+                    Array.Resize<T>(ref array, idx + 1);
+                    Console.WriteLine($"Array Resized : {array.Length}");
+                }
+                array[idx] = value;
+            }
+        }
+        public int Length { get { return array.Length; } }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                yield return array[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                yield return array[i];
+            }
+        }
+        public T Current { get { return array[position]; } }
+        object IEnumerator.Current { get { return array[position]; } }
+        public void Reset()
+        {
+            position = -1;
+        }
+        public bool MoveNext()
+        {
+            if (position == array.Length - 1)
+            {
+                
+            }
+        }
+    }
+    class EnumerableGeneric
+    {
+    }
+}
