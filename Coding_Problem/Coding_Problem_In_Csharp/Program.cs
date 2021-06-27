87c191ed61571a0398e72f8daf313db1a8cc7d17
윤구현 <ghyoon@bitmango.com>
2021-06-28 00:37:47 +0900

    Add | new project 'Coding Problem in c#

---
+++ Coding_Problem/Coding_Problem_In_Csharp/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Coding_Problem_In_Csharp {
+    internal class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello, new project!");
+        }
+    }
+}
